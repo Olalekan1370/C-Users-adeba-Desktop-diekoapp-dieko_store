02d35b77be259378094ffe776d546496f4c6e0b7
Odofin Oyejide <odofintimothy@comsoftltd.com>
2020-02-19 17:36:59 +0100

    added stock

--- src/main/java/timsoft/ehr/org/controller/StockController.java
+++ src/main/java/timsoft/ehr/org/controller/StockController.java
@@ -6,11 +6,19 @@
 package timsoft.ehr.org.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import timsoft.ehr.org.model.Stock;
+import timsoft.ehr.org.model.Supplier;
+import timsoft.ehr.org.repository.AppService;
+import timsoft.ehr.org.utils.AppHelper;
+import timsoft.ehr.org.utils.AppUtils;
+import timsoft.ehr.org.utils.FacesUtils;
+import timsoft.ehr.org.utils.MessageUtil;
 
 /**
  *
@@ -20,11 +28,73 @@ import timsoft.ehr.org.model.Stock;
 @Scope("session")
 public class StockController {
      private List<Stock> datalist;
+     @Autowired
+     AppService service;
+     @Autowired
+    LoginController login;
     @PostConstruct
     public void init(){
         datalist = new ArrayList<>();
     }
+public void filter(){
+    
+    AppHelper app =(AppHelper)FacesUtils.getManagedBean("appHelper");
+   datalist = service.getStockRepo()
+           .filterByDateRange(AppUtils.getDate(app.getDateFrom()), AppUtils.getDate(app.getDateTo()));
+   if(datalist.isEmpty()){
+       login.log(MessageUtil.RECORD_NOT_FOUND, MessageUtil.ERROR, MessageUtil.ERROR_TAG);
+   }
+}
+    public void reload() {
+        datalist = service.getStockRepo().findAll();
+    }
+public void search(){
+    AppHelper app =(AppHelper)FacesUtils.getManagedBean("appHelper");
+   datalist = service.getStockRepo().search(app.getSearchterm());
+   if(datalist.isEmpty()){
+       login.log(MessageUtil.RECORD_NOT_FOUND, MessageUtil.ERROR, MessageUtil.ERROR_TAG);
+   }
+}
+    public void add() {
+        try {
+            Stock sp = (Stock) FacesUtils.getManagedBean("stock");
+            sp.setDatecreated(new Date());
+           
+            service.getStockRepo().save(sp);
+            login.reset("stock");
+            login.log(MessageUtil.RECORD_CREATED, MessageUtil.SUCCESS, MessageUtil.SUCCESS_TAG);
+        } catch (Exception e) {
+            e.printStackTrace();
+            login.log(MessageUtil.INTERNAL_ERROR, MessageUtil.ERROR, MessageUtil.ERROR_TAG);
+        }
 
+    }
+    public void update() {
+        try {
+            Stock sp = (Stock) FacesUtils.getManagedBean("stock");
+            sp.setDatecreated(new Date());
+            service.getStockRepo().save(sp);
+            login.reset("stock");
+            login.log(MessageUtil.RECORD_CREATED, MessageUtil.SUCCESS, MessageUtil.SUCCESS_TAG);
+            reload();
+        } catch (Exception e) {
+            e.printStackTrace();
+            login.log(MessageUtil.INTERNAL_ERROR, MessageUtil.ERROR, MessageUtil.ERROR_TAG);
+        }
+
+    }
+    
+    public void delete(Long id){
+        try{
+            service.getStockRepo().delete(id);
+            login.log(MessageUtil.RECORD_DELETED, MessageUtil.SUCCESS, MessageUtil.SUCCESS_TAG);
+            reload();
+        }catch(Exception e){
+             e.printStackTrace();
+            login.log(MessageUtil.INTERNAL_ERROR, MessageUtil.ERROR, MessageUtil.ERROR_TAG);
+        }
+    }
+    
     public List<Stock> getDatalist() {
         return datalist;
     }
