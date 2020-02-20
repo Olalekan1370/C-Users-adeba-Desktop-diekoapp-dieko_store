75e6b7af8a152e74d960fe36d6f02db7a974f0d6
Sunkanmi <olasunkanmizik@gmail.com>
2020-02-19 16:47:57 -0800

    added stock

---
+++ src/main/java/timsoft/ehr/org/controller/StockController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timsoft.ehr.org.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import timsoft.ehr.org.model.Stock;
+
+/**
+ *
+ * @author HP
+ */
+public class StockController {
+     private List<Stock> datalist;
+    @PostConstruct
+    public void init(){
+        datalist = new ArrayList<>();
+    }
+
+    public List<Stock> getDatalist() {
+        return datalist;
+    }
+
+    public void setDatalist(List<Stock> datalist) {
+        this.datalist = datalist;
+    }
+        
+
+    
+}
