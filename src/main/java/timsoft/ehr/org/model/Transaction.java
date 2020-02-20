3e7d9f97630bafb894708abcbc7b89290b8847e0
Sunkanmi <olasunkanmizik@gmail.com>
2020-02-19 16:27:03 -0800

    added transaction

--- src/main/java/timsoft/ehr/org/model/Transaction.java
+++ src/main/java/timsoft/ehr/org/model/Transaction.java
@@ -7,6 +7,8 @@ package timsoft.ehr.org.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -26,7 +28,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  *
  * @author JIDEX
+ * 
  */
+@ManagedBean
+@ViewScoped
 @Entity
 @Table(name = "transaction")
 @XmlRootElement
